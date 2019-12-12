39f0b056fcdc0f07fe6ea0108af1555774e3574f
Blueblueblob <adrien.lesot@gmail.com>
2019-12-12 10:17:19 +0000

    dump version

--- custom_components/gtasks/const.py
+++ custom_components/gtasks/const.py
@@ -2,7 +2,7 @@
 # Base component constants
 DOMAIN = "gtasks"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.2.0"
+VERSION = "0.3.1"
 PLATFORMS = ["sensor", "binary_sensor"]
 REQUIRED_FILES = [
     ".translations/en.json",
